7fec10cbdde70d5a9ffa443de03f1f28b52854ea
shaido987 <shaido987@hotmail.com>
2021-12-08 15:56:57 +0800

    Update evaluate.py
    Minor fixes for easier evaluation

--- evaluate.py
+++ evaluate.py
@@ -33,7 +33,7 @@ def evaluate(root_casues, label):
 
     pred_labels = []
     for rc in root_casues:
-        elems = np.array([d + '=' for d in rc['cuboid']], dtype=np.object) + np.array(rc['elements'], dtype=np.object)
+        elems = np.array([d + '=' for d in rc['cuboid']], dtype=object) + np.array(rc['elements'], dtype=object)
         print('elems', elems)
         pred_labels.extend(['&'.join(e) for e in elems])
 
@@ -53,35 +53,34 @@ def evaluate(root_casues, label):
 
 def evaluate_folder(main_folder, attributes, method, run_folder=0, debug=False):
     folders = os.listdir(main_folder)
-    for folder in folders[run_folder:run_folder+1]:
-        print('Folder', folder)
+    folder = folders[run_folder]:
 
-        label_file = os.path.join(main_folder, folder, 'injection_info.csv')
-        labels = pd.read_csv(label_file)
+    label_file = os.path.join(main_folder, folder, 'injection_info.csv')
+    labels = pd.read_csv(label_file)
 
-        files = os.listdir(os.path.join(main_folder, folder))
-        total_TP, total_FP, total_FN = 0, 0, 0
-        for i, file in enumerate(files[:-1]):  # Skip the label file
-            print('file number', i, ', file', file)
-            timestamp = int(os.path.splitext(file)[0])
-            label = labels.loc[labels['timestamp'] == timestamp, 'set'].iloc[0]
-            data = pd.read_csv(os.path.join(main_folder, folder, file))
-            
-            cluster_root_causes = run_method(data, attributes, method, debug)
+    files = os.listdir(os.path.join(main_folder, folder))
+    total_TP, total_FP, total_FN = 0, 0, 0
+    for i, file in enumerate(files[:-1]):  # Skip the label file
+        print('file number', i, ', file', file)
+        timestamp = int(os.path.splitext(file)[0])
+        label = labels.loc[labels['timestamp'] == timestamp, 'set'].iloc[0]
+        data = pd.read_csv(os.path.join(main_folder, folder, file))
 
-            TP, FP, FN = evaluate(cluster_root_causes, label, debug)
-            if FP > 0 or FN > 0:
-                print('TP:', TP, 'FP:', FP, 'FN:', FN)
-            total_TP += TP
-            total_FP += FP
-            total_FN += FN
+        cluster_root_causes = run_method(data, attributes, method, debug)
 
-        F1 = 2 * total_TP / (2 * total_TP + total_FP + total_FN)
+        TP, FP, FN = evaluate(cluster_root_causes, label)
+        if FP > 0 or FN > 0:
+            print('TP:', TP, 'FP:', FP, 'FN:', FN)
+        total_TP += TP
+        total_FP += FP
+        total_FN += FN
 
-        print('total_TP:', total_TP)
-        print('total_FP:', total_FP)
-        print('total_FN:', total_FN)
-        print('F1-score:', F1)
+    F1 = 2 * total_TP / (2 * total_TP + total_FP + total_FN)
+
+    print('total_TP:', total_TP)
+    print('total_FP:', total_FP)
+    print('total_FN:', total_FN)
+    print('F1-score:', F1)
         
 
 if __name__ == "__main__":
